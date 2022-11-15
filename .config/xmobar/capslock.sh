7989717522d5b8360fcb4ef587c7983368fa4516
Ether <etherrorcode404@gmail.com>
2022-11-16 02:29:02 +0600

    Update capslock.sh

--- .config/xmobar/capslock.sh
+++ .config/xmobar/capslock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 state=$(xset -q | grep Caps | awk '{print $4}')
 if [ "$state" = "on" ]; then
-    echo "[<fc=#d3869b>CapsLock [A]</fc>]"
+    echo "[<fc=#d3869b>CapsLock (A)</fc>]"
 else
     echo ""
 fi
