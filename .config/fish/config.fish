3113dab85897673a8c6008268a41d93d9dc0f818
Ether <etherrorcode404@gmail.com>
2022-11-24 02:08:46 +0600

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     set fish_greeting
     export PATH="$HOME/.cargo/bin:$PATH"
+    set --universal nvm_default_version lts
     alias ls='exa --long --color auto --icons --sort=type'
     alias la='exa -a --long --color auto --icons --sort=type'
 end
