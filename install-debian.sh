f5a19c88c1fc1d28e9413a4dcb1c57bc685115a0
Ether <etherrorcode404@gmail.com>
2022-12-29 23:45:45 +0600

    fix alacritty issue

--- install-debian.sh
+++ install-debian.sh
@@ -77,12 +77,26 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 rustup override set stable
 rustup update stable
-cargo install alacritty
 wget https://raw.githubusercontent.com/alacritty/alacritty/master/extra/linux/Alacritty.desktop
 sudo mkdir -p /usr/local/share/applications/
 sudo mv Alacritty.desktop /usr/local/share/applications/
 sudo chmod +X /usr/local/share/applications/Alacritty.desktop
 sudo chmod 775 /usr/local/share/applications/Alacritty.desktop
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+cargo build --release --no-default-features --features=x11
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+sudo mkdir -p /usr/local/share/man/man1
+gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+mkdir -p ~/.bash_completion
+cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
+echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
+mkdir -p $fish_complete_path[1]
+cp extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
 
 echo "## Xmonad ##"
 sleep 2s
