0c6a816c2730d3e608a8ec8d41447714dd1fca46
Ether <etherrorcode404@gmail.com>
2023-01-02 14:20:01 +0600

    Update battery.sh

--- .config/xmobar/battery.sh
+++ .config/xmobar/battery.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 ac=" "
-state=$(upower -i $(upower -e | grep '/battery') | grep --color=never -E state)
-plugged=$(echo "${state}" | grep -wo "charging")
-charge=$(upower -i $(upower -e | grep '/battery') | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//)
+
+if [ -d /proc/acpi/battery/BAT* ]; then
+ state=$(upower -i $(upower -e | grep '/battery') | grep --color=never -E state)
+ plugged=$(echo "${state}" | grep -wo "charging")
+ charge=$(upower -i $(upower -e | grep '/battery') | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//)
+else
+    echo "<fc=#d3869b>$ac AC power</fc>"
+fi
+
 case $((charge/50)) in
   0) icon=" " ;;
   1) icon=" " ;;
