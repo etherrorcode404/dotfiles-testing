49ccde8f07c68925f3078655546942c0cf309a45
Ether <etherrorcode404@gmail.com>
2023-04-16 21:36:42 +0600

    Update install-wl.sh

--- install-wl.sh
+++ install-wl.sh
@@ -22,11 +22,6 @@ echo "#####################################################################"
 echo "## Updating and installing 'Script Dependencies' if not installed  ##"
 echo "#####################################################################"
 sudo apt-get install -qq -f -y dialog grep wget
-sudo wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/etherrorcode404/dotfiles/main/sources.list"
-sudo chmod +x /etc/apt/sources.list
-sudo chmod 775 /etc/apt/sources.list
-
-sudo apt-get update && sudo apt-get dist-upgrade -qq -f -y
 
 echo "#####################################"
 echo "## Adding 'non-free' and 'contrib' ##"
@@ -126,51 +121,6 @@ sudo ninja -C build install
 echo "## Waybar ##"
 sudo apt-get install waybar
 
-echo "## Spotify & adblock ##"
-sleep 2s
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo gpg --dearmor -o /usr/share/keyrings/spotify-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/spotify-archive-keyring.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo nala update && sudo nala install spotify-client
-sudo rm -rf /usr/local/share/applications/spotify.desktop
-git clone https://github.com/abba23/spotify-adblock.git
-cd spotify-adblock
-make
-sudo make install
-cd $HOME
-mkdir -p ~/.local/share/applications
-sudo wget -O ~/.local/share/applications/spotify.desktop "https://raw.githubusercontent.com/etherrorcode404/dotfiles/main/spotify.desktop"
-sudo chmod +x ~/.local/share/applications/spotify.desktop
-sudo chmod 775 ~/.local/share/applications/spotify.desktop
-curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
-
-echo "## Brave Browser ##"
-sleep 2s
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt-get update && sudo apt-get install -qq -f -y brave-browser
-
-echo "## Git-Hub Desktop ##"
-wget -qO - https://mirror.mwt.me/ghd/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
-sudo sh -c 'echo "deb [arch=amd64] https://mirror.mwt.me/ghd/deb/ any main" > /etc/apt/sources.list.d/packagecloud-shiftkey-desktop.list'
-sudo apt-get update && sudo apt-get install -qq -f -y github-desktop
-
-echo "## Neovim ##"
-sleep 2s
-sudo apt-get autoremove -qq -y neovim
-sudo apt-get autoremove -qq -y neovim-runtime
-wget -O nvim.deb "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb"
-sudo nala install ./nvim.deb
-
-echo "## Zoom ##"
-sleep 2s
-wget -O zoom.deb "https://zoom.us/client/latest/zoom_amd64.deb"
-sudo nala install ./zoom.deb
-
-echo "## Discord ##"
-sleep 2s
-wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-sudo nala install ./discord.deb
-
 echo "## Shell ##"
 sudo apt-get install fish
 
@@ -187,6 +137,6 @@ sleep 2s
 echo "## Clean UP ##"
 sleep 2s
 rm -rf *.deb
-rm -rf gruvbox-material-gtk spotify-adblock alacritty 
+rm -rf gruvbox-material-gtk 
 sudo apt-get autopurge
 sudo apt-get update && sudo apt-get dist-upgrade
