a58e237b52b3d41077d6c9550e0ab990625f52d8
Ether <etherrorcode404@gmail.com>
2023-04-16 21:06:58 +0600

    Delete .config/xmobar directory

--- .config/xmobar/capslock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-state=$(xset -q | grep Caps | awk '{print $4}')
-if [ "$state" = "on" ]; then
-    echo "[<fc=#d3869b>CapsLock (A)</fc>]"
-else
-    echo ""
-fi
--- .config/xmobar/date.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo "<fc=#8ec07c> $(date +"%I:%M %p %a %d/%b/%y")</fc>"
