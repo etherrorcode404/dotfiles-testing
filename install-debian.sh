497153dfc89fc867f50b870169aa64d53e7bd1e7
Ether <etherrorcode404@gmail.com>
2023-04-16 20:58:18 +0600

    Update install-debian.sh

--- install-debian.sh
+++ install-debian.sh
@@ -42,7 +42,7 @@ echo "########################"
 echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
 echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-sudo apt-get update && sudo apt-get install -qq -f -y nala-legacy
+sudo apt-get update && sudo apt-get install -qq -f -y nala
 
 echo "######################"
 echo "## Updating Mirrors ##"
@@ -51,7 +51,7 @@ sudo nala fetch
 echo installing the pre-requisites..
 sleep 2s
 egrep -v "^$|^[[:space:]]*#" $(pwd)/pkglist > pkginstall
-sudo apt-get install $(cat pkginstall)
+sudo apt-get install $(cat pkginstall) && sudo apt-get build-dep wlroots
 
 echo "##################################"
 echo "## Building and adding Programs ##"
@@ -171,8 +171,8 @@ wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=d
 sudo nala install ./discord.deb
 
 echo "## Shell ##"
-echo "Installing fish from backports.."
-sudo apt-get install -qq -f -y fish/bullseye-backports
+sudo apt-get install fish
+
 echo "symlink in /bin/sh:"
 readlink /bin/sh
 echo "symlink in /usr/bin/sh:"
@@ -189,108 +189,3 @@ rm -rf *.deb
 rm -rf gruvbox-material-gtk spotify-adblock alacritty picom
 sudo apt-get autopurge
 sudo apt-get update && sudo apt-get dist-upgrade
-
-echo "## Drivers ##"
-
-BLUE='\033[0;34m'
-GREEN='\033[0;32m'
-RED='\033[1;31m'
-
-extra=$(lspci -nnk | grep -i -EA3 "3d|display|vga" | tail -n 2)
-
-echo -e "${GREEN} $extra \n"
-echo 'Choose video drivers to install you may choose multiple'
-echo 'e.g; Driver to install: xserver-xorg-intel xserver-xorg-amdgpu'
-echo -e "${BLUE} Some Options: xserver-xorg-video-amdgpu/ati/radeon/intel/nvidia/all \n"
-read -p 'Drvier to install: ' pkgs
-echo 'Choosen:' $pkgs
-sudo nala install $pkgs
-
-# Prompt to install proprietary drivers
-if [ $pkgs == "xserver-xorg-video-amdgpu" ]; then
-    echo -e "${BLUE} Would you like to install proprietary drivers? \n"
-    read -p '(Y/N):' proprietarydrivers
-    if [ $proprietarydrivers = "Y" ]; then
-        sudo nala install firmware-amd-graphics
-    fi
-elif [ $pkgs == "xserver-xorg-video-radeon" ]; then
-    echo -e "${BLUE} Would you like to install proprietary drivers? \n"
-    read -p 'Y/N: ' proprietarydrivers
-    if [ $proprietarydrivers = "Y" ]; then
-        sudo nala install firmware-amd-graphics
-    fi
-fi
-
-# Remove screen tearing
-echo -e "${BLUE} To remove screen tearing please type whatever is shown Kernel driver in use: \n"
-echo -e "${RED} $extra \n"
-read -p 'Kernel driver in use: ' kerneluse
-echo 'Choosen:' $kerneluse
-
-## AMD ##
-# radeon
-if [ $kerneluse == "radeon" ]; then
-    if [ -d "/etc/X11/" ]; then
-        echo "/etc/X11 exists"
-    else
-    sudo mkdir /etc/X11
-    fi
-touch xorg.conf && echo "Section \"Device\"
-    Identifier \"Radeon\"
-    Driver \"radeon\"
-    Option \"TearFree\" \"on\"
-EndSection" >> xorg.conf
-    FILE=/etc/X11/xorg.conf
-    if test -f "$FILE"; then
-        sudo rm -rf -v $FILE
-    fi
-    sudo mv $(pwd)/xorg.conf /etc/X11/
-    sudo chmod +x /etc/X11/xorg.conf
-    sudo chmod +775 /etc/X11/xorg.conf
-    echo "Radeon is now free of screen tearing"
-fi
-
-# amdgpu
-if [ $kerneluse == "amdgpu" ]; then
-    if [ -d "/etc/X11/xorg.conf.d" ]; then
-        echo "/etc/X11/xorg.conf.d exists"
-    else
-    sudo mkdir -p /etc/X11/xorg.conf.d
-    fi
-touch 20-amdgpu.conf && echo "Section \"Device\"
-    Identifier \"AMD\"
-    Driver \"amdgpu\"
-    Option \"TearFree\" \"true\"
-EndSection" >> 20-amdgpu.conf
-    FILE=/etc/X11/xorg.conf.d/20-amdgpu.conf
-    if test -f "$FILE"; then
-        sudo rm -rf -v $FILE
-    fi
-    sudo mv $(pwd)/20-amdgpu.conf /etc/X11/xorg.conf.d/
-    sudo chmod +x /etc/X11/xorg.conf.d/20-amdgpu.conf
-    sudo chmod +775 /etc/X11/xorg.conf.d/20-amdgpu.conf
-    echo "Amdgpu is now free of screen tearing"
-fi
-
-## Intel ##
-# intel and nvidia
-if [ $kerneluse == "intel" ]; then
-    if [ -d "/etc/X11/xorg.conf.d" ]; then
-        echo "/etc/X11/xorg.conf.d exists"
-    else
-    sudo mkdir -p /etc/X11/xorg.conf.d
-    fi
-touch 20-intel.conf && echo "Section \"Device\"
-    Identifier \"Intel Graphics\"
-    Driver \"intel\"
-    Option \"TearFree\" \"true\"
-EndSection" >> 20-intel.conf
-    FILE=/etc/X11/xorg.conf.d/20-intel.conf
-    if test -f "$FILE"; then
-        sudo rm -rf -v $FILE
-    fi
-    sudo mv $(pwd)/20-intel.conf /etc/X11/xorg.conf.d/
-    sudo chmod +x /etc/X11/xorg.conf.d/20-intel.conf
-    sudo chmod +775 /etc/X11/xorg.conf.d/20-intel.conf
-    echo "Intel is now free of screen tearing"
-fi
